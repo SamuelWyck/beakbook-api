c0874cb57b0ef8d552bdab320015bab855290566
samuel <samawyck@gmail.com>
2025-08-25 12:51:31 -0500

    Allow cors and use prisma seesions and hook up auth route

--- app.js
+++ app.js
@@ -1,18 +1,50 @@
-const express = require("express");
 require("dotenv").config();
+const express = require("express");
+const expressSession = require("express-session");
+const {PrismaSessionStore} = require("@quixo3/prisma-session-store");
+const {PrismaClient} = require("./generated/prisma");
+const passport = require("./utils/passport.js");
+const {addUserToRes} = require("./utils/authMiddleware.js");
+const cors = require("cors");
+const authRoute = require("./routes/authRoute.js");
 
 
 
 const app = express();
 
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+app.use(
+    expressSession({
+        cookie: {
+            maxAge: 7 * 24 * 60 * 60 * 1000 //one week
+        },
+        secret: process.env.SESSION_SECRET,
+        resave: true,
+        saveUninitialized: true,
+        store: new PrismaSessionStore(
+            new PrismaClient(),
+            {
+                checkPeriod: 2 * 60 * 1000, //two minutes
+                dbRecordIdIsSessionId: true,
+                dbRecordIdFunction: undefined
+            }
+        )
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(addUserToRes);
+
 
 app.get("/", function(req, res) {
-    return res.send("Hello world");
+    return res.json({user: res.user});
 });
+app.use("/auth", authRoute);
 
 
 const PORT = process.env.PORT;
