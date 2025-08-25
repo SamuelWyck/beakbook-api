c90aff14730fa63ad5988def8061a5f7b5c38cb5
samuel <samawyck@gmail.com>
2025-08-25 17:51:05 -0500

    Allow font-end domain and credentials

--- app.js
+++ app.js
@@ -13,7 +13,10 @@ const authRoute = require("./routes/authRoute.js");
 const app = express();
 
 
-app.use(cors());
+app.use(cors({
+    origin: process.env.FRONT_END_DOMAIN,
+    credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
