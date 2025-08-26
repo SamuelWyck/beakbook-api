6afb2d7ab6d5fdc67bc6663059d682f3c1126e0e
samuel <samawyck@gmail.com>
2025-08-26 17:46:50 -0500

    Create handler for getting user data

---
+++ controllers/userController.js
@@ -0,0 +1,77 @@
+const asyncHandler = require("express-async-handler");
+const db = require("../db/querys.js");
+
+
+
+const userDataGet = asyncHandler(async function(req, res) {
+    try {
+        const [globalChat, userData] = await Promise.all([
+            db.findChatRoom({
+                where: {
+                    name: "Global chat"
+                }
+            }),
+            db.findUniqueUser({
+                where: {
+                    id: req.user.id
+                },
+                include: {
+                    chatRooms: {
+                        where: {
+                            name: null
+                        },
+                        include: {
+                            users: {
+                                select: {
+                                    id: true,
+                                    username: true
+                                }
+                            }
+                        }
+                    },
+                    friends: {
+                        include: {
+                            user: {
+                                select: {
+                                    id: true,
+                                    username: true
+                                }
+                            }
+                        }
+                    },
+                    friendRequests: {
+                        include: {
+                            requestingUser: {
+                                select: {
+                                    id: true,
+                                    username: true
+                                }
+                            }
+                        }
+                    }
+                }
+            })
+        ]);
+
+        return res.json({
+            userData: {
+                user: req.user,
+                chatRooms: userData.chatRooms,
+                friends: userData.friends,
+                friendRequests: userData.friendRequests,
+                globalChat: globalChat
+            }
+        });
+
+    } catch {
+        return res.status(500).json(
+            {errors: [{msg: "Unable to fetch user data"}]}
+        );
+    }
+});
+
+
+
+module.exports = {
+    userDataGet
+};
