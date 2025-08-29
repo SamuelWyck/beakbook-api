e8ed1edad0336cf5a2a1e82bce5db2331fc91835
samuel <samawyck@gmail.com>
2025-08-29 16:51:37 -0500

    Create methods to find, edit, and delete messages

--- db/querys.js
+++ db/querys.js
@@ -29,10 +29,31 @@ async function createMessage(options) {
 };
 
 
+async function findChatMessages(options) {
+    const messages = await prisma.message.findMany(options);
+    return messages;
+};
+
+
+async function editMessage(options) {
+    const message = await prisma.message.update(options);
+    return message;
+};
+
+
+async function deleteMessage(options) {
+    const message = await prisma.message.delete(options);
+    return message;
+};
+
+
 
 module.exports = {
     findUniqueUser,
     createUser,
     findChatRoom,
-    createMessage
+    createMessage,
+    findChatMessages,
+    editMessage,
+    deleteMessage
 };
