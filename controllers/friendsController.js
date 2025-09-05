edbaf8388f5661996dd5d01972b2bf17f4d1ee54
samuel <samawyck@gmail.com>
2025-09-05 11:25:58 -0500

    Add handlers to add and delete friends

--- controllers/friendsController.js
+++ controllers/friendsController.js
@@ -21,7 +21,12 @@ const friendRequestPost = asyncHandler(async function(req, res) {
                 id: receivingUserId,
                 friends: {
                     none: {
-                        userId: userId
+                        friendId: userId
+                    }
+                },
+                friendShips: {
+                    none: {
+                       userId: userId 
                     }
                 },
                 friendRequests: {
@@ -132,10 +137,115 @@ const delFriendRequest = asyncHandler(async function(req, res) {
 
 
 
+const addFriendPost = asyncHandler(async function(req, res) {
+    if (!req.body || !req.body.requestId) {
+        return res.status(400).json(
+            {errors: [{msg: "Missing request id"}]}
+        );
+    }
+
+    const requestId = req.body.requestId;
+    const userId = req.user.id;
+
+    let request = null;
+    try {
+        request = await db.deleteFriendRequest({
+            where: {
+                id: requestId
+            }
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json(
+            {errors: [{msg: "Unable to find friend request"}]}
+        );
+    }
+    if (!request) {
+        return res.status(400).json(
+            {errors: [{msg: "Unable to find friend request"}]}
+        );
+    }
+
+    let friendLink = null;
+    try {
+        friendLink = await db.createFriend({
+            data: {
+                userId: userId,
+                friendId: request.requestingUserId
+            },
+            include: {
+                friend: {
+                    select: {
+                        username: true,
+                        profileImgUrl: true,
+                        id: true
+                    }
+                },
+                user: {
+                    select: {
+                        username: true,
+                        profileImgUrl: true,
+                        id: true
+                    }
+                }
+            }
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json(
+            {errors: [{msg: "Unable to add friend"}]}
+        );
+    }
+    if (!friendLink) {
+        return res.status(400).json(
+            {errors: [{msg: "Unable to add friend"}]}
+        );
+    }
+
+    return res.json({friendLink});
+});
+
+
+
+const delFriend = asyncHandler(async function(req, res) {
+    if (!req.params || !req.params.relationId) {
+        return res.status(400).json(
+            {errors: [{msg: "Missing relation id"}]}
+        );
+    }
+
+    const relationId = req.params.relationId;
+
+    let relation = null;
+    try {
+        relation = await db.deleteFriend({
+            where: {
+                id: relationId
+            }
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json(
+            {errors: [{msg: "Unable to find friend"}]}
+        );
+    }
+    if (!relation) {
+        return res.status(400).json(
+            {errors: [{msg: "Unable to find friend"}]}
+        ); 
+    }
+
+    return res.json({relation});
+});
+
+
+
 module.exports = {
     friendRequestPost: [
         friendRequestVal,
         friendRequestPost
     ],
-    delFriendRequest
+    delFriendRequest,
+    addFriendPost,
+    delFriend
 };
