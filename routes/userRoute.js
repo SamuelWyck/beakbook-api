97ade58dbc52a6bda779fa211f8b17d0005cccc2
samuel <samawyck@gmail.com>
2025-09-12 09:29:16 -0500

    Create user/delete-image route

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -6,11 +6,13 @@ const {isLoggedIn} = require("../utils/authMiddleware.js");
 
 const userRoute = Router();
 
+userRoute.use(isLoggedIn);
 
-userRoute.get("/", isLoggedIn, userController.userDataGet);
+userRoute.get("/", userController.userDataGet);
 userRoute.post("/upload-image", userController.userImageUpload);
 userRoute.get("/profile", userController.userProfileDataGet);
 userRoute.put("/change-password", userController.changeUserPassword);
+userRoute.delete("/delete-image", userController.userImageDelete);
 
 
 
